e2c514e896120f418a10817e5821f678cb8d05b7
Abhinav Chaudhary <abhinav@technosip.com>
2022-03-08 22:59:04 +0530

    core session and register user with current user

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,14 @@
 class Users::SessionsController < Devise::SessionsController
 	respond_to :json
-  
+	
 	private
   
 	def respond_with(resource, _opts = {})
-	  render json: { message: 'You are logged in.' }, status: :ok
+	  render json: {
+		   message: 'You are logged in.', 
+		   user: current_user
+		}, 
+		   status: :ok
 	end
   
 	def respond_to_on_destroy
